9d83aa7f0a36078113f7add3b2225dbedb4e2c62
Habtamu <habtamugirum0489@gmail.com>
2024-01-03 10:53:11 +0300

    update

--- osquery_script.ps1
+++ osquery_script.ps1
@@ -1,47 +1,55 @@
-# Define the osquery queries and their simplified names as a dictionary
-$osquery_queries = @{
-    "SELECT REPLACE(CONCAT(hostname, '-', uuid), '-', '_') AS unique_id FROM system_info;" = "unique_id"
+# Define the osquery queries and their simplified names as a hashtable
+
+$osqueryQueries = @{
+    "SELECT REPLACE(CONCAT(hostname, '-', uuid), '-', '_') AS uniqueId FROM system_info;" = "uniqueId"
     "SELECT name, version FROM os_version;" = "os_version"
     "SELECT name FROM programs WHERE name LIKE '%avast%' OR name LIKE '%antivirus%';" = "antivirus_programs"
     "SELECT CASE WHEN protection_status = 0 THEN 'Hard drive not encrypted' ELSE 'Hard drive encrypted' END AS encryption_status FROM bitlocker_info LIMIT 1;" = "bitlocker_status"
     "SELECT CASE WHEN name IN ('LastPass', '1Password', 'Bitwarden', 'Dashlane', 'Keeper', 'RoboForm', 'NordPass', 'Enpass', 'Sticky Password', 'Password Safe', 'Myki', 'RememBear') THEN name ELSE 'No password manager in use' END AS password_manager FROM programs WHERE name IN ('LastPass', '1Password', 'Bitwarden', 'Dashlane', 'Keeper', 'RoboForm', 'NordPass', 'Enpass', 'Sticky Password', 'Password Safe', 'Myki', 'RememBear') UNION SELECT 'No password manager in use' AS password_manager WHERE (SELECT COUNT(*) FROM programs WHERE name IN ('LastPass', '1Password', 'Bitwarden', 'Dashlane', 'Keeper', 'RoboForm', 'NordPass', 'Enpass', 'Sticky Password', 'Password Safe', 'Myki', 'RememBear')) = 0;" = "password_manager"
-    "SELECT username FROM users WHERE directory LIKE 'C:\Users\%';" = "usernames"
-    "SELECT name, data FROM registry WHERE key = 'HKEY_CURRENT_USER\Control Panel\Desktop' AND (name = 'ScreenSaveActive' OR name = 'ScreenSaverIsSecure');" = "screen Lock"
+    "SELECT username FROM users WHERE directory LIKE 'C:\\Users\\%';" = "usernames"
+    "SELECT name, data FROM registry WHERE key = 'HKEY_CURRENT_USER\\Control Panel\\Desktop' AND (name = 'ScreenSaveActive' OR name = 'ScreenSaverIsSecure');" = "screen_lock"
 }
 
 # Define the API endpoint where you want to send the data
-$endpoint_url = "https://api.vistar.cloud/api/v1/computers/osquery_log_data/"
+$endpointUrl = "https://api.vistar.cloud/api/v1/computers/osquery_log_data/"
 
 # Specify the interval (in seconds) between data sends (e.g., every 1 hour)
-$interval_seconds = 3600  # 1 hour
+$intervalSeconds = 3600  # 1 hour
 
 while ($true) {
     # Initialize an empty hashtable to store the results of all queries
-    $all_osquery_data = @{}
+    $allOsqueryData = @{}
 
     # Run each osquery query and capture the output
-    foreach ($query in $osquery_queries.Keys) {
+    foreach ($query in $osqueryQueries.GetEnumerator()) {
+        $queryText = $query.Key
+        $simplifiedName = $query.Value
+
         try {
-            $osquery_output = Invoke-Expression "C:\Program Files\osquery\osqueryi.exe --json $query"
-            $osquery_data = $osquery_output | ConvertFrom-Json
-            $all_osquery_data[$osquery_queries[$query]] = $osquery_data
-        } catch {
-            Write-Host "Error running osquery for query '$query': $_"
+            $osqueryOutput = & 'C:\Program Files\osquery\osqueryi.exe' "--json" $queryText
+            $osqueryData = $osqueryOutput | ConvertFrom-Json
+            $allOsqueryData[$simplifiedName] = $osqueryData
+        }
+        catch {
+            Write-Host "Error running osquery for query '$queryText': $_"
         }
     }
 
     # Send the combined data to the endpoint using the Invoke-RestMethod cmdlet
     try {
-        $response = Invoke-RestMethod -Uri $endpoint_url -Method Post -Body ($all_osquery_data | ConvertTo-Json) -ContentType "application/json"
+        $response = Invoke-RestMethod -Uri $endpointUrl -Method Post -Body ($allOsqueryData | ConvertTo-Json) -ContentType 'application/json'
+
         if ($response.StatusCode -eq 201) {
             Write-Host "Data sent successfully. Status code: 201"
-        } else {
+        }
+        else {
             Write-Host "Failed to send data. Status code: $($response.StatusCode)"
         }
-    } catch {
+    }
+    catch {
         Write-Host "Error sending data: $_"
     }
 
     # Sleep for the specified interval before the next run
-    Start-Sleep -Seconds $interval_seconds
+    Start-Sleep -Seconds $intervalSeconds
 }
